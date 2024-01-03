c5f078eb72eaa55d70970a1596d25480668517c6
ten4roid <46445480+xoxlo@users.noreply.github.com>
2024-01-03 13:35:16 +0900

    Add files via upload

---
+++ workspace/day01/src/exam01/Task01.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Task01 {
+	public static void main(String[] args) {
+		
+//		주석 : 프로그램에 영향을 미치지 않음
+//		한줄 주석 단축키 : Ctrl + /
+//		한줄 주석 해제 단축키 : Ctrl + /
+//		여러줄 주석 단축키 : Ctrl + Shift + /
+
+	}
+	
+}
