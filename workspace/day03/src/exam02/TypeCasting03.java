1d4d311391af74862adf79c2f57a9c13ea436969
ten4roid <46445480+xoxlo@users.noreply.github.com>
2024-01-05 13:36:31 +0900

    Add files via upload

---
+++ workspace/day03/src/exam02/TypeCasting03.java
@@ -0,0 +1,12 @@
+package exam02;
+
+public class TypeCasting03 {
+	public static void main(String[] args) {
+
+//		int, double, char, boolean -> 기본자료형, 원시자료형
+//		String, 클래스, ... -> 참조자료형, 클래스 자료형
+		
+//		기본 자료형 -> String
+		String str1 = 3+"";
+	}
+}
