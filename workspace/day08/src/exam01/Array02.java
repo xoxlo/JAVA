653df2dec1ed53ec87534b5aa0c4883a836792bf
ten4roid <46445480+xoxlo@users.noreply.github.com>
2024-01-12 13:24:33 +0900

    Add files via upload

---
+++ workspace/day08/src/exam01/Array02.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public class Array02 {
+	public static void main(String[] args) {
+		
+//		<2차원 배열>
+//		2차원 배열 선언
+		int[][] arr1 = new int[2][3]; // 2행 열의 배열 생성
+		
+		int[][] arr2 = {{1, 2, 3},
+						{4, 5, 6}};
+		
+	}
+}
